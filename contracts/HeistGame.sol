ad39400e47ca24077e315947daf880394d77024d
DDA209 <didier.pascarel1@gmail.com>
2026-01-08 11:55:02 +0100

    initialising a specific card

--- contracts/HeistGame.sol
+++ contracts/HeistGame.sol
@@ -5,4 +5,20 @@ import * as Types from "./HeistTypes.sol";
 
 contract HeistGame{
     // Do something cool
+    Types.NftCard public bossCard; // Avec public, le compilateur crée automatiquement une fonction de lecture (getter)
+    
+    constructor() {
+        bossCard = Types.NftCard(
+            Types.Rarities.GodLike, // TODO:doit être calculée grace aux skills, cost et fees
+            Types.Skills.alarmDeactivater,
+            Types.Skills.cyberHacker,
+            Types.Ability(
+                Types.Abilities.patient, 
+                true
+                ),
+            17700, // TODO: à récupérer dans un tableau de concordence (voir documentation du projet du jeu)
+            5 // TODO: à récupérer dans un tableau de concordence (voir documentation du projet du jeu)
+        );
+    }
+
 }
